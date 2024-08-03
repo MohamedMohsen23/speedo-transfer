f76aeb7bd068b4bd48846fba93c75a1ee4033222
Youssef_Gad <youssef.gad2003@yahoo.com>
2024-08-03 18:57:29 +0300

    Add Auth Folders

---
+++ src/app/shared/models/user.model.ts
@@ -0,0 +1,9 @@
+export class User {
+  constructor(
+    public username: string,
+    public password: string,
+    public email: string,
+    public country: string,
+    public dataOfBirth: string
+  ) {}
+}
